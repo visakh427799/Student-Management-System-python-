6a885cb7de28b644fe48b935b5f0a1e4d77a7862
visakh427799 <visakhsanthosh69@gmail.com>
2021-06-15 11:00:07 +0530

    routes

--- Student Management System/app.py
+++ Student Management System/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
-
+from flask import Flask,render_template
+from routes import admin
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+    return "Home"
 
-app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+ app.run(debug=True)
